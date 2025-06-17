a7fbadad8f248a3d5bcb0677541386ef098eac17
miles-ph9 <milesmouni@gmail.com>
2025-06-16 22:47:36 -0400

    Simple Calculator file

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,12 @@
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
+        System.out.println("Multiplication: " + multiply(a, b));
+
+        }
+    }
