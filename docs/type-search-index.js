f9165212460f87f334e8ee842010592a50f6eeef
Gideon <gideonnega@gmail.com>
2025-06-25 20:34:02 -0400

    Added java docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"Add"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"Multiply"},{"p":"<Unnamed>","l":"SimpleCalculator"},{"p":"<Unnamed>","l":"Subtract"}];updateSearchResults();
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+/**
+ * This a main class to test Person.java
+ *
+ * @author Shiva Sharma
+ * @version 1.0
+ * @see Multiply
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
